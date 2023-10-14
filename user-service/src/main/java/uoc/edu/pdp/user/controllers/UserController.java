1dced674dfe39039ddc6ce0c79452e35d815ed47
jmuletpol <jmuletpol@uoc.edu>
2023-10-14 16:37:26 +0200

    Eureka server

---
+++ user-service/src/main/java/uoc/edu/pdp/user/controllers/UserController.java
@@ -0,0 +1,16 @@
+package uoc.edu.pdp.user.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String greetings() {
+        return "Hi from user service";
+    }
+
+}
